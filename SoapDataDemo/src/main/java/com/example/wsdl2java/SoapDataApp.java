ab258dba9c8714dafa3365e3bbff4e5bc51fe31a
NhlahlaMathye <Nhlahla.Mathye@hellogroup.co.za>
2022-05-23 23:26:31 +0200

    new project

---
+++ SoapDataDemo/src/main/java/com/example/wsdl2java/SoapDataApp.java
@@ -0,0 +1,63 @@
+package com.example.wsdl2java;
+
+import com.example.wsdl2java.SoapDataUtils;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class SoapDataApp {
+
+    static boolean checkInfo;
+    static int programLevel = 0;
+
+    public static void main(String[] args) throws MalformedURLException
+    {
+
+        checkInfo = false;
+        while (programLevel != 9)
+        {
+            SoapDataApp.mainProgramData(programLevel);
+        }
+    }
+
+
+    private static void mainProgramData(int inputUser) throws InputMismatchException, MalformedURLException {
+        SoapDataUtils dataUtils = new SoapDataUtils();
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            switch (inputUser){
+
+                case 0:
+                    System.out.println("Select number for information you want to retrieve" +
+                            "\n" +
+                            "\n 1. List of languages" +
+                            "\n 2. List of countries" +
+                            "\n" +
+                            "\n Enter (9) to exit program");
+                    programLevel = sc.nextInt();
+                    break;
+                case 1:
+                    dataUtils.getLanguagesByName();
+                    programLevel = 0;
+                    break;
+
+                case 2:
+                    dataUtils.getCountriesByName();
+                    programLevel = 0;
+                    break;
+
+                default:
+                    break;
+            }
+        }catch (InputMismatchException e)
+        {
+            System.out.println("Expects a number!!");
+        }
+
+    }
+}
